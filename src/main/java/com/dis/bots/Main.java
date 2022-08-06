9569b4d26057a460bf38fde7551abfc2aabc24a3
Felipe Faria <felipe.faria@waitrose.co.uk>
2022-08-06 11:53:42 +0100

    Basic hello world from Discord4J

--- src/main/java/com/dis/bots/Main.java
+++ src/main/java/com/dis/bots/Main.java
@@ -1,7 +1,25 @@
 package com.dis.bots;
 
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.MessageChannel;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final String token = System.getenv("BOT_TOKEN");
+        final DiscordClient client = DiscordClient.create(token);
+        final GatewayDiscordClient gateway = client.login().block();
+
+        gateway.on(MessageCreateEvent.class).subscribe(event -> {
+            final Message message = event.getMessage();
+            if ("!ping".equals(message.getContent())) {
+                final MessageChannel channel = message.getChannel().block();
+                channel.createMessage("Pong!").block();
+            }
+        });
+
+        gateway.onDisconnect().block();
     }
 }
