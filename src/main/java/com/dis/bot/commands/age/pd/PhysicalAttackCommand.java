da3ec6dd144c1a2f268310cea0e01efcded44a49
Felipe Faria <felipe.faria@waitrose.co.uk>
2022-08-11 02:36:00 +0100

    Created physical attack command for 13thage rpg

---
+++ src/main/java/com/dis/bot/commands/age/pd/PhysicalAttackCommand.java
@@ -0,0 +1,42 @@
+package com.dis.bot.commands.age.pd;
+
+import com.dis.bot.commands.SlashCommand;
+import com.dis.bot.service.age.PhysicalDefenseService;
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import static com.dis.bot.tool.EventOptionNameGetter.getEventOption;
+import static com.dis.bot.tool.EventOptionNameGetter.getEventOptionAsLong;
+
+@Component
+public class PhysicalAttackCommand implements SlashCommand {
+
+    private final PhysicalDefenseService service;
+
+    public PhysicalAttackCommand(PhysicalDefenseService service){
+        this.service = service;
+    }
+
+    @Override
+    public String getName() {
+        return "physical-attack";
+    }
+
+    @Override
+    public Mono<Void> handle(ChatInputInteractionEvent event) {
+        String foeName = getEventOption(event, "name");
+        Long attackBonus = getEventOptionAsLong(event, "bonus");
+
+        var result = service.hit(foeName, attackBonus);
+
+        return  event.reply()
+            .withEphemeral(false)
+            .withContent(String.format("%s was hit %s. (Physical Attack total score %d, [roll %d, bonus %d])",
+                    result.getTargetName(),
+                    result.isHit() ? "successfully" : "unsuccessfully",
+                    result.getTotalHitValue(),
+                    result.getRoll(),
+                    result.getAttackBonus()));
+    }
+}
