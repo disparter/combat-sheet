98520a4732b7786a016e3a1c998c175842d49f07
Felipe Faria <felipe.faria@waitrose.co.uk>
2022-09-05 23:52:48 +0100

    Fixing mongo express docker compose configurations

--- src/main/java/com/dis/bot/repository/mongo/AgeCharacterRepository.java
+++ src/main/java/com/dis/bot/repository/mongo/AgeCharacterRepository.java
@@ -2,6 +2,8 @@ package com.dis.bot.repository.mongo;
 
 import com.dis.bot.model.age.AgeCharacter;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AgeCharacterRepository extends MongoRepository<AgeCharacter, String> {
 }
